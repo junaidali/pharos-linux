9a332076f3782cd308cd5304427a34b1d2bd47c3
Junaid Ali <mailtojunaid@gmail.com>
2012-02-10 10:30:58 -0600

    Added ending message to inform user to restart GUI session

--- setup.py
+++ setup.py
@@ -415,6 +415,7 @@ def main():
 	# Install Uninstaller
 	installUninstaller()
 	
+	print('\nIIT Remote printing has been successfully installed on your computer. Please restart your GUI session to complete the installation process. The simplest way to do this is to log out and log back in!')
 
 # Main Script ============================
 
